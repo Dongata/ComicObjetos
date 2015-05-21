e1d35f73c57890008bd1b301257b152a07e26792
Gaston <Gaston@LenoCat>
2015-05-20 22:46:43 -0300

    funcionalidad dada

--- src/comicobjetos/GreaterBattleCriteria.java
+++ src/comicobjetos/GreaterBattleCriteria.java
@@ -5,14 +5,13 @@ import comicobjetos.entities.ComicElement;
 public class GreaterBattleCriteria extends BattleCriteria
 {       
     @Override
-    public  ComicElement battle(ComicElement char1, ComicElement char2)
-	{
-            if(char1.getStat(stat)>char2.getStat(stat))
-                return char1;
-            if(char1.getStat(stat)<char2.getStat(stat))
-                return char2;
-            return null;
-	}
+    public  ComicElement battle(ComicElement char1, ComicElement char2){
+        if(char1.getStat(stat)>char2.getStat(stat))
+            return char1;
+        if(char1.getStat(stat)<char2.getStat(stat))
+            return char2;
+        return null;
+    }
 
 
 
--- src/comicobjetos/MinorBattleCriteria.java
+++ src/comicobjetos/MinorBattleCriteria.java
@@ -9,7 +9,11 @@ public class MinorBattleCriteria extends BattleCriteria
 
     @Override
     public ComicElement battle(ComicElement char1, ComicElement char2) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(char1.getStat(stat)<char2.getStat(stat))
+            return char1;
+        if(char1.getStat(stat)>char2.getStat(stat))
+            return char2;
+        return null;
     }
 
 }
