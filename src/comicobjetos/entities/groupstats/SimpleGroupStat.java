8246aa1268a28a6e464bb9fd21bd7f294c23955e
Carolina <Carolina@CaroLaptop>
2015-05-20 23:11:40 -0300

    modifique a tu hermana

--- src/comicobjetos/entities/groupstats/SimpleGroupStat.java
+++ src/comicobjetos/entities/groupstats/SimpleGroupStat.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class SimpleGroupStat extends GroupStatCalculator
 {
     private float value;
+    int caro;
 
     @Override
     public float calculate(List<Float> elements) {
