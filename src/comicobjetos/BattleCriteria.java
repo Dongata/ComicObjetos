5801ba8105ff291511b6c49aafdbe2316981f680
Carolina <Carolina@CaroLaptop>
2015-05-28 21:16:10 -0300

    El ordenar está un poquito más bonito y eliminé una función en BattleCriteria que no se usa.

--- src/comicobjetos/BattleCriteria.java
+++ src/comicobjetos/BattleCriteria.java
@@ -11,12 +11,4 @@ public abstract class BattleCriteria{
 
     public abstract ComicElement battle(ComicElement char1, ComicElement char2);
 
-    public int doCompare(Object o, Object o1){
-        ComicElement ce = battle((ComicElement)o, (ComicElement)o1);
-        if(ce.equals(o)) return 1;
-        if(ce.equals(o1))return -1;
-        return 0;
-    }
-
 }
-
--- src/comicobjetos/Sorter.java
+++ src/comicobjetos/Sorter.java
@@ -17,19 +17,15 @@ public class Sorter
         ArrayList<ComicElement> orderedList = new ArrayList<>(elements.size());
         orderedList.add(0, elements.get(0));
         ComicElement best = null;
-        int k = 0;
-        int j = 0;
+        int k = 0, j = 0;
+        
         for (int i=1; i<elements.size(); i++){
             while (j<orderedList.size()){          
                 while ((k<criterias.size()) && (best == null)){
                     best = criterias.get(k).battle(elements.get(i), orderedList.get(j));
                     k++;
                 }
-                if (best == null){
-                    orderedList.add(j,elements.get(i));
-                    j = orderedList.size();
-                }
-                else if (best == orderedList.get(j)){
+                if ((best == null) || (best == orderedList.get(j))){
                     orderedList.add(j,elements.get(i));
                     j = orderedList.size();
                 }
